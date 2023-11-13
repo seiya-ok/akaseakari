998ae2395a717b8655572826c2be003298cfb81e
seiya-ok <cbso1210108@gn.iwasaki.ac.jp>
2023-11-14 05:59:25 +0900

    記事投稿作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,21 +4,20 @@
         <meta charset="utf-8">
         <title>Blog</title>
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+       <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+       
     </head>
-    <body>
+    <body class="antialiased">
         <h1>Blog Name</h1>
+        <a href='/posts/create'>create</a>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'><a href="/posts/{{ $post->id}}">{{ $post->title }}</a>
-                    </h2>
+                    <a href="/posts/{{ $post->id}}"><h2 class='title'>{{ $post->title }}</h2></a>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
         </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
-        </div>
+        <div class='paginate'>{{ $posts->links() }} </div>
     </body>
 </html>
