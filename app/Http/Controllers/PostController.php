4205e376dad749da364cf1edbb405c0603379a4f
seiya-ok <cbso1210108@gn.iwasaki.ac.jp>
2023-11-14 15:15:53 +0900

    エラーの見える化

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -19,7 +20,7 @@ public function create()
 {
     return view('posts.create');
 }
-public function store(Request $request, Post $post)
+public function store(Post $post, PostRequest $request)
 {
     $input = $request['post'];
     $post->fill($input)->save();
